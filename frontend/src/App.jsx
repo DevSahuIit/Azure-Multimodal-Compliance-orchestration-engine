7db4c5aa71b9de8251abab30f7aee159a77b8f57
Dev Sahu <devsahu2966@gmail.com>
2026-07-22 05:27:10 +0530

    making form

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AuditFormApp from './AuditFormApp';
+
+export default function App() {
+  return (
+    <main className="min-h-screen bg-slate-950 flex flex-col justify-center items-center">
+      <AuditFormApp />
+    </main>
+  );
+}
